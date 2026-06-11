b8286d09fedf0a4c22f7691c6ab5f5d1f495b2e3
daemunozr <dae.munozr@duocuc.cl>
2026-06-10 20:36:19 -0400

    pequeños cambios

--- src/main/java/com/sivebo/ms_sucursales/controller/ComunaController.java
+++ src/main/java/com/sivebo/ms_sucursales/controller/ComunaController.java
@@ -1,11 +1,15 @@
 package com.sivebo.ms_sucursales.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.sivebo.ms_sucursales.model.Comuna;
 import com.sivebo.ms_sucursales.repository.ComunaRepository;
+
 import lombok.RequiredArgsConstructor;
-import java.util.List;
 
 @RestController
 @RequestMapping("/comunas")
--- src/main/java/com/sivebo/ms_sucursales/controller/RegionController.java
+++ src/main/java/com/sivebo/ms_sucursales/controller/RegionController.java
@@ -1,11 +1,15 @@
 package com.sivebo.ms_sucursales.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.sivebo.ms_sucursales.model.Region;
 import com.sivebo.ms_sucursales.repository.RegionRepository;
+
 import lombok.RequiredArgsConstructor;
-import java.util.List;
 
 @RestController
 @RequestMapping("/regiones")
