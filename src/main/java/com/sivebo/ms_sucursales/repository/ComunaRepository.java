a2f74db1733f5894b8db1007e4c26d89a5ffc3cc
daemunozr <dae.munozr@duocuc.cl>
2026-06-13 11:32:34 -0400

    arreglado ComunaRepository

--- src/main/java/com/sivebo/ms_sucursales/repository/ComunaRepository.java
+++ src/main/java/com/sivebo/ms_sucursales/repository/ComunaRepository.java
@@ -1,9 +1,14 @@
 package com.sivebo.ms_sucursales.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.sivebo.ms_sucursales.model.Comuna;
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sivebo.ms_sucursales.model.Comuna;
 
 public interface ComunaRepository extends JpaRepository<Comuna, Long> {
     List<Comuna> findByRegionId(Long regionId);
+
+    Optional<Comuna> findByNombre(String nombre);
 }
