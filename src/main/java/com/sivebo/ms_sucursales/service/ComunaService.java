326bc40e159f728e1388e46150fe88440ff3fb30
daemunozr <dae.munozr@duocuc.cl>
2026-06-13 13:01:07 -0400

    comunaService arreglado

--- src/main/java/com/sivebo/ms_sucursales/service/ComunaService.java
+++ src/main/java/com/sivebo/ms_sucursales/service/ComunaService.java
@@ -1,1 +1,50 @@
-0
+package com.sivebo.ms_sucursales.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.sivebo.ms_sucursales.dto.ComunaResponseDTO;
+import com.sivebo.ms_sucursales.repository.ComunaRepository;
+import com.sivebo.ms_sucursales.utils.MapToDTO;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ComunaService extends MapToDTO {
+
+        private final ComunaRepository comunaRepository;
+
+        public List<ComunaResponseDTO> getAllComunas() {
+                return comunaRepository.findAll()
+                        .stream()
+                        .map(this::mapComunaToDTO)
+                        .collect(Collectors.toList());
+        }
+
+        public Optional<ComunaResponseDTO> getComunaById(Long id) {
+                return comunaRepository.findById(id).map(this::mapComunaToDTO);
+        }
+
+        public Optional<ComunaResponseDTO> getComunaByNombre(String nombre) {
+                return comunaRepository.findByNombre(nombre).map(this::mapComunaToDTO);
+        }
+
+        public List<ComunaResponseDTO> getComunasByRegionId(Long idRegion) {
+                return comunaRepository.findByRegionId(idRegion)
+                        .stream().map(this::mapComunaToDTO)
+                        .collect(Collectors.toList());
+        }
+
+        public List<ComunaResponseDTO> getComunasByRegionNombre(String nombreRegion) {
+                return comunaRepository.findByRegionNombre(nombreRegion)
+                        .stream().map(this::mapComunaToDTO)
+                        .collect(Collectors.toList());
+        }
+
+}
