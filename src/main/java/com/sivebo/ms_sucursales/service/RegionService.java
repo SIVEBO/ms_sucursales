f7d75a521411bcb9127e13ea2471da3bce4d2406
daemunozr <dae.munozr@duocuc.cl>
2026-06-13 13:02:09 -0400

    RegionService arreglado

--- src/main/java/com/sivebo/ms_sucursales/service/RegionService.java
+++ src/main/java/com/sivebo/ms_sucursales/service/RegionService.java
@@ -4,10 +4,18 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.springframework.stereotype.Service;
+
 import com.sivebo.ms_sucursales.dto.RegionResponseDTO;
 import com.sivebo.ms_sucursales.repository.RegionRepository;
 import com.sivebo.ms_sucursales.utils.MapToDTO;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class RegionService extends MapToDTO {
     
         private final RegionRepository regionRepository;
