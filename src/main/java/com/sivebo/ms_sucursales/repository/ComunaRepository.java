d31904eb1fae01fcd8083cb0972a79a6ac585b4a
daemunozr <dae.munozr@duocuc.cl>
2026-06-13 12:29:13 -0400

    Actualizado repositories. Modificado y unificado Service

--- src/main/java/com/sivebo/ms_sucursales/repository/ComunaRepository.java
+++ src/main/java/com/sivebo/ms_sucursales/repository/ComunaRepository.java
@@ -10,5 +10,7 @@ import com.sivebo.ms_sucursales.model.Comuna;
 public interface ComunaRepository extends JpaRepository<Comuna, Long> {
     List<Comuna> findByRegionId(Long regionId);
 
+    List<Comuna> findByRegionNombre(String regionNombre);
+
     Optional<Comuna> findByNombre(String nombre);
 }
--- src/main/java/com/sivebo/ms_sucursales/repository/SucursalRepository.java
+++ src/main/java/com/sivebo/ms_sucursales/repository/SucursalRepository.java
@@ -11,6 +11,12 @@ public interface SucursalRepository extends JpaRepository<Sucursal, Long> {
 
         List<Sucursal> findByComunaId(Long comunaId);
 
+        List<Sucursal> findByComunaNombre(String comunaNombre);
+
         Optional<Sucursal> findByNombre(String nombre);
+
+        void deleteByNombre(String nombre);
+
+        Boolean existsByNombre(String nombre);
         
 }
